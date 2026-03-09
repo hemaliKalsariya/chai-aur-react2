60d4dbe40509aba98fde06ef598601dcec6a5d4e
hemaliKalsariya <hemukalsariya7705@gmail.com>
2026-03-09 08:57:18 +0000

    props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,23 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
- import  Card from './components/Card'
+import Cards from './cards/Cards'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-     <>
-    <h1 className=' bg-green-500 text-black p-4 rounded-xl'>Tailwind test</h1>
-    <Card />
-  
+    <>
+      <h1 className="text-3xl font-bold  bg-green-500 text-white p-4">Tailwind</h1>
+      <Cards username="Sweet Dreams" />
+      <Cards  />
 
-
+      {/* <img width={400} height={300} className='object-cover object-center w-100 h-full ' src="https://images.pexels.com/photos/3585034/pexels-photo-3585034.jpeg" alt="Featured content" /> */}
     </>
   )
- }
+}
 
-export default App 
-
-
-
+export default App
